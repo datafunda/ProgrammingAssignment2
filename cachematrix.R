a73f5fb968d9c7279efb391514537eb0404b99a4
Prateek Singhal <prateeksinghal11@yahoo.co.in>
2014-08-24 02:18:16 +0800

    Assignment 2
    Assignment 2

---
+++ cachematrix.R
@@ -0,0 +1,27 @@
+makeCacheMatrix <- function(x=matrix()) {
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<-inverse
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+  
+}
+
+cacheSolve <- function(x, ...) {
+
+  m <- x$getInverse()
+  if ( ! is.null(m)) {
+    print("getting cached data")
+    return(m)
+  }
+  m <- solve(x$get())
+  x$setInverse(m)
+  m
+}
